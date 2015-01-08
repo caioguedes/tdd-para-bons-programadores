cec99300e23bd726525d14d4919aebe081f49cc4
Caio César <caio.cesar.g.souza@gmail.com>
2015-01-07 22:10:06 -0200

    Adding a new test

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -32,6 +32,20 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function testPlayStartWith60()
+    {
+        $this->assertEquals(
+            [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz',
+            'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16, 17,
+            'fizz', 19, 'buzz', 'fizz', 22, 23, 'fizz', 'buzz',
+            26, 'fizz', 28, 29, 'fizzbuzz', 31, 32, 'fizz',
+            34, 'buzz', 'fizz', 37, 38, 'fizz', 'buzz', 41,
+            'fizz', 43, 44, 'fizzbuzz', 46, 47, 'fizz', 49, 'buzz',
+            'fizz', 52, 53, 'fizz', 'buzz', 56, 'fizz', 58, 59, 'fizzbuzz'],
+            $this->fb->play(60)
+        );
+    }
+
     public function testMultiples5ReturnBuzz()
     {
         $this->assertEquals(
